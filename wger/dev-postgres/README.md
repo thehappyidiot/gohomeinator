6ef59979256ef1b4a6143876ef0438084d0ef152
Meer Shah <meer@hey.com>
2026-07-29 10:58:31 -0500

    remove wger

--- wger/dev-postgres/README.md
+++
@@ -1,3 +0,0 @@
-# Dev environment for wger
-
-Please consult <https://wger.readthedocs.io/en/latest/development/docker.html> for details
--- wger/dev-postgres/initdb/02-cleanup.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-TRUNCATE TABLE exercises_exerciseimage;
-TRUNCATE TABLE exercises_exercisevideo;
-TRUNCATE TABLE nutrition_image;
-
-COMMIT;
